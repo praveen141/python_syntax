a10bca024f1e92dd0286938ee3d25c8f9727c28f
praveen <praveen.gupta141@gmail.com>
2026-07-10 04:54:46 +0530

    added set

--- Chapter1/main.py
+++ Chapter1/main.py
@@ -4,3 +4,14 @@ df = pandas.DataFrame({"a" : 1}, index=[1])
 import os
 print(os.listdir())
 print(os.getcwd())
+
+text = "123"
+
+print(text.isalpha())
+print(text.isdigit())
+print(text.isalnum())
+
+name = input("What is yours friends name")
+work = input("where you work")
+live = input(where yours friends live)
+
---
+++ Chapter1/sets_basic.py
@@ -0,0 +1,9 @@
+numbers = {1, 2, 2, 3, 3, 4}
+print(numbers)
+
+#sets do not support slicing because they are unordered collections of unique elements
+
+items = {"apple", "banana", "orange"}
+
+for item in items:
+    print(item)
